82bbb25ef7ca619c830fd3bef681bdd1d69b244b
Cayo Legal <cayolegal@hotmail.com>
2024-10-22 21:00:13 -0300

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  rules: {
+    quotes: ["error", "double"],
+    "jsx-quotes": ["error", "prefer-double"],
+    "react-hooks/exhaustive-deps": "off",
+    "react-native/no-inline-styles": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    curly: "off",
+    radix: "off",
+    "@typescript-eslint/no-shadow": "off",
+    "react-hooks/rules-of-hooks": "off",
+    "react/no-unstable-nested-components": "off",
+  },
 };
