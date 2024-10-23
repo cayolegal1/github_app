c700a152062945f54c6b8b8a531ea57416404ffe
Cayo Legal <cayolegal@hotmail.com>
2024-10-23 15:32:44 -0300

    fix alias bug

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
       "module-resolver",
       {
         alias: {
-          "@/*": "./src/*",
+          "@": "./src/",
         },
         extensions: [".js", ".jsx", ".ts", ".tsx"],
         root: ["./"],
