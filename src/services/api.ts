b958f39aa2f5c6bad8da19338faab76561032dfe
Cayo Legal <cayolegal@hotmail.com>
2024-10-24 23:58:19 -0300

    add visual fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -16,9 +16,6 @@ export class ApiFetch {
       ...axiosConfig,
       method,
       url,
-      headers: {
-        Authorization: "token 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",
-      },
     });
 
     return data;
